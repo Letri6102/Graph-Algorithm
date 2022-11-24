cb63c6ad6e8b6b4dd8bec07dd372ed1c87249b4c
Le Cong Tri <75474571+Letri6102@users.noreply.github.com>
2022-11-24 21:41:33 +0700

    Delete Prim-Kruskal directory

--- Prim-Kruskal/js/Edge.js
+++
@@ -1,9 +0,0 @@
-class Edge {
-	constructor(vertexA, vertexB, weight, color, lineWeight) {
-		this.vertexA = vertexA;
-		this.vertexB = vertexB;
-		this.weight = weight;
-		this.color = color;
-		this.lineWeight = lineWeight;
-	}
-}
